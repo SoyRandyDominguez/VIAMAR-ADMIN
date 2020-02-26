65ad6237638b3ecf6e824cffcd155850a3132792
Jancer Rodriguez <jancer.fabian@gmail.com>
2020-02-26 08:37:32 -0400

    todo correcto, antes de adivinar con el date component

--- ViaCloud/ClientApp/src/app/app.module.ts
+++ ViaCloud/ClientApp/src/app/app.module.ts
@@ -46,7 +46,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         NavigationComponent,
         BreadcrumbComponent,
         SidebarComponent,
-        LoginComponent
+        LoginComponent 
     ],
     imports: [ 
         JwtModule.forRoot({
